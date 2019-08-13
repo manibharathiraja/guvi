bfc2ba174f300d2994d42c5d92b61085369fadb8
manibharathiraja <54059514+manibharathiraja@users.noreply.github.com>
2019-08-13 22:08:33 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,9 @@
+ch=input()
+if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+    if((ch=='a') or (ch=='A') or (ch=='e') or (ch=='E') or (ch=='i') or (ch=='I') or (ch=='o') or (ch=='O') or (ch=='u') or (ch=='U')):
+        print("Vowel")
+    else:
+        print("Consonant")
+else:
+    print("invalid")
+    
